8d221b0691d325479547271d8def560b0d62f60c
RoamingHeart <aggoden@gmail.com>
2023-05-08 20:37:41 -0400

    added code for quote of the day

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,23 @@ var voicelist = responsiveVoice.getVoices();
 //pause and resume
 responsiveVoice.pause();
 responsiveVoice.resume();
+
+//quote of the day
+const url = 'https://quotel-quotes.p.rapidapi.com/quotes/random';
+const options = {
+	method: 'POST',
+	headers: {
+		'content-type': 'application/json',
+		'X-RapidAPI-Key': '48f5798bd1mshc87c9f81137df26p13b2e8jsn92f7d104c439',
+		'X-RapidAPI-Host': 'quotel-quotes.p.rapidapi.com'
+	},
+	body: {}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
