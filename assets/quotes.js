5ab6f5bcdc49cfe996f70e39d71a4b7f92eead18
Zhang <kzb11128@gmail.com>
2023-05-11 19:45:10 -0400

    fixed-savedQuote

--- assets/quotes.js
+++ assets/quotes.js
@@ -31,32 +31,27 @@ function getQuote(){
 getQuote();
 
 
-
-
-
-
-//copy function
-btnPlaceholder.addEventListener("click", () => {
-	navigator.clipboard.writeText(placeHolder.value)
-  });
-
-
 // Save quote functionality
 var saveQuoteBtn = document.querySelector("#saveQuote");
 var savedQuotesSection = document.querySelector("#savedQuotes-container");
-var textarea = document.querySelector("textarea");
+var textareaEL = document.querySelector("textarea");
 var savedQuotesList = document.querySelector("#savedQuotes")
 
-saveQuoteBtn.addEventListener('click', function(){
-	
-	var quote = textarea.getAttribute('placeholder');
-	var quoteList = document.createElement('li');
-	quoteList.textContent = quote;
-	savedQuotesList.appendChild(quoteList);
-	localStorage.setItem('mySavedQuote', quoteList.textContent);
-});
-
-var savedQuotes = localStorage.getItem('mySavedQuote');
-if(savedQuotes) {
-	savedQuotesSection.innerHTML = savedQuotes;
+// Retrieve the saved quotes from localStorage
+var savedQuotes = localStorage.getItem('mySavedQuotes');
+if (savedQuotes) {
+    savedQuotesList.innerHTML = savedQuotes;
 }
+
+saveQuoteBtn.addEventListener('click', function(){
+
+    var quote = textareaEL.getAttribute('placeholder');
+    var quoteList = document.createElement('li');
+// set the quote as the text content of the new li element
+    quoteList.textContent = quote; 
+// append the new li element to the saved quotes list    
+    savedQuotesList.appendChild(quoteList); 
+
+    // Save the updated saved quotes list to localStorage
+    localStorage.setItem('mySavedQuotes', savedQuotesList.innerHTML);
+});
