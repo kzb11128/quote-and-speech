e702dc6e8fc93f985330933d9f88548826bf1161
Zhang <kzb11128@gmail.com>
2023-05-15 21:05:13 -0400

    added a copy quote button and replace the original copybtn function

--- assets/quotes.js
+++ assets/quotes.js
@@ -35,7 +35,6 @@ function getQuote(){
 // RUNS GET QUOTE FUNCTION
 getQuote();
 
-
 // Save quote functionality
 var saveQuoteBtn = document.querySelector("#saveQuote");
 
@@ -51,53 +50,45 @@ if (savedQuotes) {
 
 // Save quotes to the quotes section
 saveQuoteBtn.addEventListener('click', function(){
-var quote = textareaEL.getAttribute('placeholder');
-var quoteList = document.createElement('div');
-// Colors for background of quote containers
-let quotesBgColors = ["bg-slate-50", "bg-slate-100", "bg-gray-50", "bg-gray-100", "bg-zinc-50", "bg-zinc-100", "bg-neutral-50", "bg-neutral-100", "bg-stone-50", "bg-stone-100",
+  var quote = textareaEL.getAttribute('placeholder');
+  var quoteList = document.createElement('div');
+  // Colors for background of quote containers
+  let quotesBgColors = ["bg-slate-50", "bg-slate-100", "bg-gray-50", "bg-gray-100", "bg-zinc-50", "bg-zinc-100", "bg-neutral-50", "bg-neutral-100", "bg-stone-50", "bg-stone-100",
                             "bg-red-50", "bg-red-100", "bg-orange-50", "bg-orange-100", "bg-amber-50", "bg-amber-100", "bg-yellow-50", "bg-yellow-100", "bg-lime-50", "bg-lime-100",
                             "bg-green-50", "bg-green-100", "bg-emerald-50", "bg-emerald-100", "bg-teal-50", "bg-teal-100", "bg-cyan-50", "bg-cyan-100", "bg-sky-50", "bg-sky-100",
                             "bg-blue-50", "bg-blue-100", "bg-indigo-50", "bg-indigo-100", "bg-violet-50", "bg-violet-100", "bg-purple-50", "bg-purple-100", "bg-fuchsia-50",
                             "bg-fuchsia-100", "bg-pink-50", "bg-pink-100", "bg-rose-50", "bg-rose-100"];
 
-// Assign attributes to the quote containers
-var newBgColor = quotesBgColors[Math.floor(Math.random()*44)];
-let textBgColor = newBgColor + " max-h-64 w-fit text-sm 2xl:text-base text-gray-900 rounded-md shadow-md shadow-[#542745]/40 border border-gray-300 m-2.5 p-2.5 overflow-y-auto";
-quoteList.setAttribute("class", textBgColor);
+  // Assign attributes to the quote containers
+  var newBgColor = quotesBgColors[Math.floor(Math.random()*44)];
+  let textBgColor = newBgColor + " max-h-64 w-fit text-sm 2xl:text-base text-gray-900 rounded-md shadow-md shadow-[#542745]/40 border border-gray-300 m-2.5 p-2.5 overflow-y-auto";
+  quoteList.setAttribute("class", textBgColor);
 
-// Button attributes
-var copyBtn = document.createElement('button');
-var copyImg = document.createElement('i');
-copyImg.setAttribute('class', 'fas fa-copy')
-copyBtn.setAttribute('class', 'btns');
-copyBtn.appendChild(copyImg);
+  // set the quote as the text content of the new div element
+  quoteList.textContent = quote; 
 
-// set the quote as the text content of the new div element
-quoteList.textContent = quote; 
+  // append the new div element to the saved quotes section
+  savedQuotesList.appendChild(quoteList); 
 
-// append the new div element to the saved quotes section
-savedQuotesList.appendChild(quoteList); 
+  // Save the updated saved quotes to localStorage
+  localStorage.setItem('mySavedQuotes', savedQuotesList.innerHTML);
 
-// Save the updated saved quotes to localStorage
-localStorage.setItem('mySavedQuotes', savedQuotesList.innerHTML);
-quoteList.appendChild(copyBtn);
-copyBtn.addEventListener("click", () => {
-navigator.clipboard.writeText(quoteList.textContent);
-});
 });
 
 // Button to clear local storage and saved quotes section on the webpage
 var clearQuoteBtn = document.querySelector("#clearQuotes");
+
 clearQuoteBtn.addEventListener('click', function(){
-var quotesText = document.getElementById("savedQuotes-container");
-while (quotesText.hasChildNodes()) {
-quotesText.removeChild(quotesText.firstChild);
-}
-localStorage.clear();
+  var quotesText = document.getElementById("savedQuotes-container");
+  while (quotesText.hasChildNodes()) {
+  quotesText.removeChild(quotesText.firstChild);
+  }
+  localStorage.clear();
 });
 
-var useQuote = document.querySelector("#useQuote");
+var copyQuote = document.querySelector("#copyQuote");
 
-useQuote.addEventListener('click', function() {
+copyQuote.addEventListener('click', function() {
   textareaEL.value = textareaEL.placeholder;
+  navigator.clipboard.writeText(textareaEL.value);
 });
